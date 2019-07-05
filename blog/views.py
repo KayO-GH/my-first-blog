f2092742c4d147236470ed0f6506200c32fe5874
Kwadwo Agyapon-Ntra <kwadwo.agyapon-ntra@meltwater.org>
2019-07-05 09:22:14 +0000

    COmpleted publishing feature for unpublished posts

--- blog/views.py
+++ blog/views.py
@@ -21,7 +21,7 @@ def post_new(request):
             post.author = request.user
             #post.published_date = timezone.now()
             post.save()
-            return redirect('post_detail', pk = post.pk)
+            return redirect('post_detail', pk=post.pk)
     else:
         #form is opened for the first time
         form = PostForm()
@@ -44,3 +44,8 @@ def post_edit(request, pk):
 def post_draft_list(request):
     drafts = Post.objects.filter(published_date__isnull=True).order_by('-created_date')#order by created_date in reverse to have most recent at the top
     return render(request, 'blog/post_draft_list.html',{'drafts':drafts})
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
